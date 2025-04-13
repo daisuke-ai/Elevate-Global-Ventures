4d888f69fd7a666ee5a19e1f757f16987e6181d9
daisuke-ai <ammarv67@gmail.com>
2025-04-13 20:34:32 +0500

    footer number

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,7 +32,10 @@ const Footer: React.FC = () => {
                 <Mail className="w-5 h-5 mr-2" /> info@elevateglobalventures.com
               </li>
               <li className="flex items-center">
-                <Phone className="w-5 h-5 mr-2" /> +1 (808) 757-4053
+                <Phone className="w-5 h-5 mr-2" /> +1 424-622-6464
+              </li>
+              <li className="flex items-center">
+                <Phone className="w-5 h-5 mr-2" /> +1 424-678-1356
               </li>
               <li className="flex items-center">
                 <MapPin className="w-5 h-5 mr-2" /> Honolulu, HI
